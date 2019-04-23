0479e4b3faad34af27c941cff6372bc21328108e
aordonez <aordonez@kanteron.com>
2019-04-23 10:45:27 +0200

    Se añade reviewers del commit (1)

--- Viewers/Packages/ohif-core/server/configMysql.js
+++ Viewers/Packages/ohif-core/server/configMysql.js
@@ -3,5 +3,5 @@ import { Meteor } from 'meteor/meteor';
 Meteor.mysqlHost = "localhost";
 Meteor.mysqlPort="3306";
 Meteor.mysqlUser="root";
-Meteor.mysqlPassword="panadero";
+Meteor.mysqlPassword="kanteron";
 Meteor.mysqlDatabase="tmis";
