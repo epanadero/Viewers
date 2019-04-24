6b1da92d18df4fd2220f077678f70e08f9ea43ee
aordonez <aordonez@kanteron.com>
2019-04-24 17:34:24 +0200

    Integrar commit (7) Remove APP_NAME from routes.js - ee6b549db58db354922a733619133935f75d4e12

--- Viewers/OHIFViewer/client/routes.js
+++ Viewers/OHIFViewer/client/routes.js
@@ -7,8 +7,6 @@ import { moment } from 'meteor/momentjs:moment';
 console.log(Meteor.settings);
 
 
-var APP_NAME=Meteor.settings.public.appNameVisor;
-
 Router.configure({
     layoutTemplate: 'layout',
 });
@@ -77,39 +75,35 @@ Router.onBeforeAction('loading');
 
 
 
-Router.route(APP_NAME+'/', function() {
+Router.route('/', function() {
     Router.go('studylist', {}, { replaceState: true });
 }, { name: 'home' });
 
-Router.route(APP_NAME+'/studylist', function() {
+Router.route('/studylist', function() {
     this.render('ohifViewer', { data: { template: 'studylist' } });
 }, { name: 'studylist' });
 
-Router.route(APP_NAME+'/viewer/:studyInstanceUids', function() {
+Router.route('/viewer/:studyInstanceUids', function() {
     const studyInstanceUids = this.params.studyInstanceUids.split(';');
     OHIF.viewerbase.renderViewer(this, { studyInstanceUids }, 'ohifViewer');
 }, { name: 'viewerStudies' });
 
-Router.route(APP_NAME+'/logout', function() {
+Router.route('/logout', function() {
 
     Session.clear('userLogin');
     //delete Session.clearPersistent();
     Router.go('login', {}, { replaceState: true });
 },{name:'logout'});
 
+Router.route('/login', function(url) {
 Router.route(APP_NAME+'/login', function(url) {
     this.render('login');
 },{name:'login'});
 
-Router.route(APP_NAME+'/viewer/:studyInstanceUids/:userInstance/:passwordInstance', function() {
-    const studyInstanceUids = this.params.studyInstanceUids.split(';');
-    const user = this.params.userInstance;
-    const password = this.params.passwordInstance;
-}, { name: 'viewerStudiesWithLogin' });
 
 
 // OHIF #98 Show specific series of study
-Router.route(APP_NAME+'/study/:studyInstanceUid/series/:seriesInstanceUids', function () {
+Router.route('/study/:studyInstanceUid/series/:seriesInstanceUids', function () {
     const studyInstanceUid = this.params.studyInstanceUid;
     const seriesInstanceUids = this.params.seriesInstanceUids.split(';');
     OHIF.viewerbase.renderViewer(this, { studyInstanceUids: [studyInstanceUid], seriesInstanceUids }, 'ohifViewer');
